8cab215f6f8eb0733e7a25424b18be6d166ff071
Niels Abels <n.abels@coolblue.nl>
2017-12-23 08:01:19 +0100

    Add capability to transform into NFA

--- chapter3/regex/concatenate.rb
+++ chapter3/regex/concatenate.rb
@@ -1,5 +1,22 @@
 class Concatenate < Struct.new(:first, :second) 
     include Pattern
+
+    def to_nfa_design
+        first_nfa = first.to_nfa_design
+        second_nfa = second.to_nfa_design
+
+        start_state = first_nfa.start_state
+        accept_states = second_nfa.accept_states
+        
+        rules_free_moves = first_nfa.accept_states.map { |state| FARule.new(state, nil, second_nfa.start_state) }
+        
+        rule = FARule.new(first_nfa.accept_states[0], nil, second_nfa.start_state)
+        rules = first_nfa.rulebook.rules + second_nfa.rulebook.rules + rules_free_moves
+
+        NFADesign.new(start_state, accept_states, NFARulebook.new(rules))
+    end
+
+
     def to_s
         [first, second].map { |pattern| pattern.bracket(precedence) }.join
     end
