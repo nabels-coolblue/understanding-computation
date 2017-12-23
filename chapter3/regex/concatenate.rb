e28155eaee28390e7d4e841973d4593823ccd159
Niels Abels <n.abels@coolblue.nl>
2017-12-23 10:39:50 +0100

    Removes redundant line

--- chapter3/regex/concatenate.rb
+++ chapter3/regex/concatenate.rb
@@ -8,9 +8,7 @@ class Concatenate < Struct.new(:first, :second)
         start_state = first_nfa.start_state
         accept_states = second_nfa.accept_states
         
-        rules_free_moves = first_nfa.accept_states.map { |state| FARule.new(state, nil, second_nfa.start_state) }
-        
-        rule = FARule.new(first_nfa.accept_states[0], nil, second_nfa.start_state)
+        rules_free_moves = first_nfa.accept_states.map { |state| FARule.new(state, nil, second_nfa.start_state) }        
         rules = first_nfa.rulebook.rules + second_nfa.rulebook.rules + rules_free_moves
 
         NFADesign.new(start_state, accept_states, NFARulebook.new(rules))
