e750705f2b4c69172de9bcba4b19d6a2bee4767b
Niels Abels <n.abels@coolblue.nl>
2018-05-02 13:12:33 +0200

    Correction

--- chapter4/003.pda_configuration.rb
+++ chapter4/003.pda_configuration.rb
@@ -1,13 +1,20 @@
 # Summary
 # 
-# A PDA (Pushdown Deterministic Automaton) is a type of deterministic automaton that makes use of a stack.
+# A PDA (Pushdown Automaton) is a type of automaton that makes use of a stack.
 # The stack allows the automaton to have a, albeit of limited functionality, infinite amount of resources.
 # We can use a PDA to implement a routine that checks whether the braces of a certain input string are
 # balanced, up until an arbitrary depth, very much unlike DFAs and NFAs (001.nfa_nesting_limits.rb).
 # 
 # In this example however, we will be putting together a pda_configuration, which is the state to which
-# the configuration applies, combined with a stack of characters which need to be pushed in case the 
-# rule and configuration match.
+# the configuration applies, combined with the current stack (of characters).
+#
+# Understanding computation on the pda_configuration:
+# But there are two important things to know about a pushdown automaton at each step of
+# its computation: what its current state is, and what the current contents of its stack are.
+# If we use the word configuration to refer to this combination of a state and a stack, we
+# can talk about a pushdown automaton moving from one configuration to another as
+# it reads input characters, which is easier than always having to refer to the state and
+# stack separately.
 
 require_relative 'data_structures/_all'
 require_relative 'pda/_all'
