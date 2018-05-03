b5942c1b2429865e9cdd07f0198ec7d14c4e52f3
Niels Abels <n.abels@coolblue.nl>
2018-05-03 11:35:35 +0200

    Adds functionality to move machine to the next state, by following rules

--- chapter4/pda/pda_rule.rb
+++ chapter4/pda/pda_rule.rb
@@ -1,3 +1,13 @@
+# Rules for pushdown automata have the following properties.
+
+# The state, character and next_state need to match the current configuration of the machine in order for the rule to be applied
+# - state           : the state the rule can act on (1, 2, 3, etc)
+# - pop_character   : needs to match the topmost stack character ('(', ')', etc)
+# - character       : needs to match the next input character
+
+# - next_state      : if the rule applies, the machine should move to the specified state
+# - push_characters : after the pop_character gets popped off the stack (when moving to the next_state), these characters are pushed
+
 class PDARule < Struct.new(:start_state, :character, :next_state, :pop_character, :push_characters)
     # A rule only applies when the machine’s state, topmost stack character, and next input
     # character all have the values it expects:
@@ -7,6 +17,17 @@ class PDARule < Struct.new(:start_state, :character, :next_state, :pop_character
             self.character == character
     end
     
+    def follow(configuration)
+        PDAConfiguration.new(next_state, next_stack(configuration))
+    end
+
+    def next_stack(configuration)
+        popped_stack = configuration.stack.pop
+
+        push_characters.reverse.
+            inject(popped_stack) { |stack, character| stack.push(character) }
+    end
+
     def to_s
         "<PDARule 
         state=#{start_state}
