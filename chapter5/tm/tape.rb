9895de1fe59c7b00f866bb116ca64ac509eadba9
Niels Abels <n.abels@coolblue.nl>
2018-06-27 14:44:31 +0200

    Fixes unintended deletion of current character

--- chapter5/tm/tape.rb
+++ chapter5/tm/tape.rb
@@ -1,25 +1,25 @@
 class Tape < Struct.new(:left, :middle, :right, :blank)
-    def inspect
-        "<Tape " + left.join + "(" + middle + ")" + right.join + ">"
-    end
+  def inspect
+    "<Tape " + left.join + "(" + middle + ")" + right.join + ">"
+  end
 
-    def move_head_left
-        _left = left[0..-2]
-        _middle = left.last
-        _right = [middle] + right
+  def move_head_left
+    _left = left[0..-2]
+    _middle = left.last || blank
+    _right = [middle] + right
 
-        Tape.new(_left, _middle, _right, blank)
-    end
+    Tape.new(_left, _middle, _right, blank)
+  end
 
-    def move_head_right
-        _left = left + [middle]
-        _middle = right.first || blank
-        _right = right.drop(1)
+  def move_head_right
+    _left = left + [middle]
+    _middle = right.first || blank
+    _right = right.drop(1)
 
-        Tape.new(_left, _middle, _right, blank)
-    end
+    Tape.new(_left, _middle, _right, blank)
+  end
 
-    def write(char)
-        Tape.new(left, char, right, blank)
-    end
+  def write(char)
+    Tape.new(left, char, right, blank)
+  end
 end
