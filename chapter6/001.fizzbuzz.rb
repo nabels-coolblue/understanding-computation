3169f7a404fcb9014689574df9d6b387584d78a2
Niels Abels <n.abels@coolblue.nl>
2018-07-02 14:48:58 +0200

    Added a comment from the book

--- chapter6/001.fizzbuzz.rb
+++ chapter6/001.fizzbuzz.rb
@@ -14,3 +14,5 @@
   end
 end
 #.each { |j| puts j } we have no way to print text to the console using only procs
+
+# Despite its simplicity, this is quite an ambitious program if we don’t have any of the features of a programming language: it creates a range, maps over it, evaluates a big conditional, does some arithmetic with the modulo operator, uses the Fixnum#zero? predicate, uses some string literals, and turns numbers into strings with Fixnum#to_s. That’s a fair amount of built-in Ruby functionality, and we’re going to have to strip it all out and reimplement it with procs.
